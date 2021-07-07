9adc6c793c24c11bdd5bf0af6f0194522d6325ae
Almas <almasaytenov@gmail.com>
2021-07-07 22:14:07 +0600

    Notebook added

--- hr_applicant/models/models.py
+++ hr_applicant/models/models.py
@@ -22,7 +22,28 @@ class hr_applicant(models.Model):
 
 	interviewer = fields.Many2one('hr.employee')
 	responsible = fields.Many2one('hr.employee')
-
+	#personal
+	applicant_gender = fields.Selection([
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('other', 'Other'),
+    ])
+	nationality = fields.Char()
+	address = fields.Char()
+	birth_date = fields.Date()
+	citizenship = fields.Char()
+	is_single = fields.Boolean()
+	#work
+	workplace = fields.Char()
+	workp_experience = fields.Boolean()
+	job_position = fields.Char()
+	work_start_date = fields.Date()
+	work_desc = fields.Text()
+	#education
+	study_place = fields.Char()
+	specialty = fields.Char()
+	admission_date = fields.Date()
+	graduation_date = fields.Date()
 
 	@api.onchange('responsible')
 	def _set_interviewer(self):
@@ -43,5 +64,3 @@ class hr_applicant(models.Model):
 				record.interviewer = record.responsible
 			else:
 				record.interviewer = record.department_id.manager_id
-
-
